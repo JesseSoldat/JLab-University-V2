f3dc1f0386c1fca86a61d2bae7b81eb44216a73e
Jesse <jsoldat@hotmail.com>
2018-04-16 18:41:21 +0700

    custom query

--- wp-content/themes/jlab-uni/readme.php
+++ wp-content/themes/jlab-uni/readme.php
@@ -1,25 +1,3 @@
 
-<?php
-bloginfo('name'); //get the site title
-bloginfo('description'); //get the site tag line
-
-$names = array('Jesse', 'Nate');
-echo $names[0];
-
-$count = 1;
-while($count <= 10) {
-  echo "<li>$count</li>";
-  $count++;
-}
-
-$count = 0;
-while($count < count($names)) {
-  echo "<li>Hi, my name is $names[$count]</li>";
-  $count++;
-}
-?>
-
-
 Pellentesque sed venenatis sapien. In interdum orci odio, vel semper sapien tristique ut. Vivamus non placerat tortor, in rutrum diam. Nam vitae vestibulum dolor. Aliquam vitae neque volutpat, accumsan neque vel, euismod nulla. Duis id urna malesuada nisl convallis vulputate. Nullam molestie erat a sapien lacinia facilisis. Vivamus dictum nec nibh sit amet luctus. Quisque ipsum erat, accumsan in dignissim ac, pharetra varius tortor. Duis volutpat elementum nibh, a mattis dui aliquet non. Donec ultricies odio in mauris aliquet, non interdum lacus feugiat. Nunc vel justo eros.
 
-Mauris vitae sagittis ante. Vestibulum eget sagittis velit. Proin sit amet tincidunt urna. Cras interdum quis lectus luctus eleifend. Nulla luctus ut metus ut ultricies. Donec scelerisque felis convallis risus tincidunt auctor. Sed finibus, velit nec cursus pharetra, massa nisl commodo lectus, eget mollis magna libero quis libero. Quisque sollicitudin nisi eu mauris tempor porta. Donec dapibus ex quis maximus sagittis. Phasellus viverra pretium ex, eu iaculis erat convallis eget.
